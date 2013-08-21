d96a89a7960449fe0616c5710f2e6b417c8cd250
Eugenio Piasini <e.piasini@ucl.ac.uk>
2013-08-21 13:57:31 +0100

    Remove keyword passing to extension, tidy up c++ code.

--- pymuvr/pymuvr.cpp
+++ pymuvr/pymuvr.cpp
@@ -7,11 +7,11 @@
 using namespace std;
 
 // prototypes
-static PyObject * distance_matrix(PyObject *self, PyObject *args, PyObject *kwds);
+static PyObject * distance_matrix(PyObject *self, PyObject *args);
 
 // method table
 static PyMethodDef pymuvrMethods[] = {
-  {"distance_matrix", (PyCFunction)distance_matrix, METH_VARARGS|METH_KEYWORDS, "Distance (dissimilarity) matrix with the Multi-Unit Van Rossum metric."},
+  {"distance_matrix", distance_matrix, METH_VARARGS, "Distance (dissimilarity) matrix with the Multi-Unit Van Rossum metric."},
   {NULL, NULL, 0, NULL}  /* Sentinel */
 };
 
@@ -24,24 +24,30 @@ initpymuvr(void) {
 
 
 // main module function
-static PyObject * distance_matrix(PyObject *self, PyObject *args, PyObject *kwds){
-  static char *kwlist[] = {"tau", "cos", NULL};
+static PyObject * distance_matrix(PyObject *self, PyObject *args){
   double tau, cos;
   PyObject *observations;
   Py_ssize_t big_n, big_p;
   PyArrayObject *py_d_matrix;
+  double *py_data;
+  npy_intp dims[2];
   double **c_d_matrix;
 
-  if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!dd", kwlist,
-				   &PyObject_Type, &observations, &tau, &cos))
+  // parse arguments
+  if (!PyArg_ParseTuple(args, "Odd",
+			&observations, &tau, &cos))
     return NULL;
   
   // build the 3D array (observation, cell, spiketime) to be fed to
-  // the original metric implementation
+  // the original metric implementation. There are big_n observations
+  // (ie "network activity realisations", or "multi-unit spike
+  // trains"), and each of them is composed by big_p single-unit spike
+  // trains, but each of the single-unit spike train can have a
+  // different length. This means the 3D array doesn't have a regular
+  // shape.
   big_n = PyList_Size(observations);
   big_p = PyList_Size(PyList_GetItem(observations, (Py_ssize_t)0));
   vector<vector<vector<double> > > trains(big_n,vector<vector<double> >(big_p));
-
   for(Py_ssize_t n=0;n<big_n;++n){
     PyObject *ob = PyList_GetItem(observations, n);
     for(Py_ssize_t p=0;p<big_p;++p){
@@ -54,13 +60,14 @@ static PyObject * distance_matrix(PyObject *self, PyObject *args, PyObject *kwds
   
   // instantiate the dissimilarity (distance) matrix where the results
   // will be stored 
-  npy_intp dims[2] = {big_n, big_n};
-  py_d_matrix = (PyArrayObject *)PyArray_SimpleNew(big_n, dims, NPY_DOUBLE);
-  int intn = (int)PyArray_DIM(py_d_matrix, 0);
-  int intm = (int)PyArray_DIM(py_d_matrix, 1);
-  double *a = (double *)PyArray_DATA(py_d_matrix); /* pointer to data as double */
-  for (unsigned int i=0; i<intn; i++){
-    c_d_matrix[i]=a+i*intm;
+  dims[0] = big_n;
+  dims[1] = big_n;
+  py_d_matrix = (PyArrayObject *)PyArray_SimpleNew(2, dims, NPY_DOUBLE);
+  py_data = (double *)PyArray_DATA(py_d_matrix); // pointer to data as double
+  c_d_matrix = new double* [big_n]; // array of pointers to the rows
+				    // of the numpy matrix
+  for (npy_intp i=0; i<big_n; i++){
+    c_d_matrix[i] = py_data + i*big_n;
   }
   
   // perform the core distance calculations
@@ -69,6 +76,8 @@ static PyObject * distance_matrix(PyObject *self, PyObject *args, PyObject *kwds
   // deallocate the memory used by the data structure needed for
   // direct access to the numpy array
   delete [] c_d_matrix;
+
+  
   return PyArray_Return(py_d_matrix);
 }
 
