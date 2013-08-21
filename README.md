42f1286bb298d97c2bbe1e63a10c85c77deab4ca
Eugenio Piasini <e.piasini@ucl.ac.uk>
2013-08-21 17:33:07 +0100

    Tidy up package. Add README and license information.

---
+++ README.md
@@ -0,0 +1,10 @@
+# pymuvr
+Multi-unit Van Rossum spike train metric. This is a kernel-based
+implementation with markage vector and precomputed exponential factor,
+as described in Houghton and Kreuz, 2012, 'On the efficient
+calculation of Van Rossum distances.'. This package provides a Python
+wrapping of the original C++ implementation given by the authors of
+the paper.
+
+## License
+This package is licensed under GPLv3. See COPYING for details.
--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('pymuvr',
-                    sources = ['pymuvr/Van_Rossum_Multiunit.cpp', 'pymuvr/pymuvr.cpp'],
-                    include_dirs = ['include'])
+module1 = Extension("pymuvr",
+                    sources = ["pymuvr/Van_Rossum_Multiunit.cpp", "pymuvr/pymuvr.cpp"])
 
-setup (name = 'pymuvr',
-       version = '0.1',
-       description = 'Multi-unit Van Rossum spike train metric',
-       ext_modules = [module1],
-       long_description = "Multi-unit Van Rossum spike train metric, kernel-based version. This is a Python wrapping of the C++ implementation given by the authors in Houghton and Kreuz, 2012, 'On the efficient calculation of Van Rossum distances.'")
+setup (name = "pymuvr",
+       version = "1.0",
+       description = "Multi-unit Van Rossum spike train metric",
+       long_description = "Multi-unit Van Rossum spike train metric, kernel-based version with markage vector and precomputed exponential factor, as described in Houghton and Kreuz, 2012, 'On the efficient calculation of Van Rossum distances.'. This is a Python wrapping of the original C++ implementation given by the authors of the paper.",
+       author = "Eugenio Piasini",
+       author_email = "e.piasini@ucl.ac.uk",
+       license = "GPLv3 or later",
+       ext_modules = [module1])
+
